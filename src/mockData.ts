a0912b89dae62fb4db2e20983c64139ea5c52f7b
Coriano Harris <corianoharrispro@gmail.com>
2024-08-08 00:25:40 -0700

    mock data

---
+++ src/mockData.ts
@@ -0,0 +1,22 @@
+export const cancerList = [
+    { value: 'Breast', label: 'Breast' },
+    { value: 'Lung', label: 'Lung' },
+    { value: 'Prostate', label: 'Prostate' },
+    { value: 'Colorectal', label: 'Colorectal' },
+    { value: 'Pancreatic', label: 'Pancreatic' },
+  ]
+  
+  export const centersList = [
+    { value: 'MD Anderson Cancer Center', label: 'MD Anderson Cancer Center' },
+    { value: 'Memorial Sloan Kettering Cancer Center', label: 'Memorial Sloan Kettering Cancer Center' },
+    { value: 'Mayo Clinic Cancer Center', label: 'Mayo Clinic Cancer Center' },
+    { value: 'Dana-Farber Cancer Institute', label: 'Dana-Farber Cancer Institute' },
+  ]
+  
+  export const donationAmountsList = [
+    { value: '10', label: '$10' },
+    { value: '20', label: '$20' },
+    { value: '40', label: '$40' },
+    { value: '50', label: '$50' },
+    { value: '100', label: '$100' },
+  ]
