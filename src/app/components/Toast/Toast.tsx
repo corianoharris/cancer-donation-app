ddbf7deaed9fd8f19d9c6b731a56ef5a23263abe
Coriano Harris <corianoharrispro@gmail.com>
2024-08-08 00:31:18 -0700

    Toast component

---
+++ src/app/components/Toast/Toast.tsx
@@ -0,0 +1,10 @@
+import { toast, ToastContainer } from 'react-toastify' 
+import React from 'react';
+
+const Toast = () => {
+    return (
+        <ToastContainer position="bottom-right" />
+    );
+};
+
+export default Toast;
